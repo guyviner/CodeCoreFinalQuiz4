81f09ec633b7c49357fc78bb8fdfe7aa547abde1
Guy Viner <gregorious@gmail.com>
2016-12-05 11:15:30 -0800

    initial commit with user auctions bids models and controllers

---
+++ spec/factories/auctions.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :auction do
+    title "MyString"
+    details "MyText"
+    end_date "2016-12-05 10:41:32"
+    reserve_price 1.5
+    user nil
+  end
+end
---
+++ spec/factories/bids.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :bid do
+    amount 1.5
+    auction nil
+  end
+end
